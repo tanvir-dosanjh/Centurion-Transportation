734a249e881fb6f1bacf031889597d52bea21981
Tanvir <129093521+tanvir-dosanjh@users.noreply.github.com>
2025-12-28 17:06:58 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Select the quote button
+const quoteButton = document.getElementById('quoteButton');
+
+// Add click event listener
+quoteButton.addEventListener('click', () => {
+  alert('Thank you for your interest! We will contact you shortly.');
+  // Optionally, redirect to a contact page:
+  // window.location.href = 'contact.html';
+});
